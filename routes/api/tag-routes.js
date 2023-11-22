b38f12b4764d0b1f93bd6d2295c1aa8ad1d1a5ca
Negatseg <tseganehnegash@gmail.com>
2023-11-21 18:29:05 -0600

    update the files in tag_routes.js file

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,61 +3,61 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
-});
-
-// router.get('/', async (req, res) => {
-//   try {
-//     // Find all tags and include associated Product data
-//     const tags = await Tag.findAll({
-//       include: [{ model: Product, through: ProductTag }],
-//     });
-//     res.json(tags);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
+// router.get('/', (req, res) => {
+//   // find all tags
+//   // be sure to include its associated Product data
 // });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    // Find all tags and include associated Product data
+    const tags = await Tag.findAll({
+      include: [{ model: Product, through: ProductTag }],
+    });
+    res.json(tags);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
-// router.get('/:id', async (req, res) => {
-//   try {
-//     // Find a single tag by its id and include associated Product data
-//     const tag = await Tag.findByPk(req.params.id, {
-//       include: [{ model: Product, through: ProductTag }],
-//     });
-
-//     if (!tag) {
-//       return res.status(404).json({ error: 'Tag not found' });
-//     }
-
-//     res.json(tag);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
+// router.get('/:id', (req, res) => {
+//   // find a single tag by its `id`
+//   // be sure to include its associated Product data
 // });
+
+router.get('/:id', async (req, res) => {
+  try {
+    // Find a single tag by its id and include associated Product data
+    const tag = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag }],
+    });
+
+    if (!tag) {
+      return res.status(404).json({ error: 'Tag not found' });
+    }
+
+    res.json(tag);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 router.post('/', (req, res) => {
   // create a new tag
 });
 
-// router.post('/', async (req, res) => {
-//   try {
-//     // Create a new tag
-//     const newTag = await Tag.create(req.body);
-//     res.status(201).json(newTag);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
+router.post('/', async (req, res) => {
+  try {
+    // Create a new tag
+    const newTag = await Tag.create(req.body);
+    res.status(201).json(newTag);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
